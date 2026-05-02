5bb7c4bafbc5ece3a40ece382b15277a9e3a7da5
Blaze <blazebyte260@gmail.com>
2026-05-02 15:05:44 -0500

    feat: export all backends from backends package

--- src/credential_bridge/backends/__init__.py
+++ src/credential_bridge/backends/__init__.py
@@ -1,1 +1,7 @@
-"""Backends module for credential-bridge secrets management."""
+# src/credential_bridge/backends/__init__.py
+from .base import BaseSecretBackend
+from .env_file import EnvFileBackend
+from .keyring import KeyringBackend
+from .vault import VaultBackend
+
+__all__ = ["BaseSecretBackend", "EnvFileBackend", "KeyringBackend", "VaultBackend"]
